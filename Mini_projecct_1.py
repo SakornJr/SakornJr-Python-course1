ed82b9f4b6768afe29fe0ba2903bfdb75aed3676
SakornJr <psakarinkhul@gmail.com>
2025-12-28 17:44:03 +0700

    [GGGGGG]

--- Mini_projecct_1.py
+++ Mini_projecct_1.py
@@ -12,7 +12,7 @@ print("Your damage is", damage_player)
 print("1 is for Attack")
 print("2 is for Increase HP")
 
-while hp_player or hp_ai <= 0:
+while hp_player or hp_ai != 0:
     
     choose = str(input("1 or 2"))
     result_player_attack = hp_ai - damage_player
@@ -20,7 +20,7 @@ while hp_player or hp_ai <= 0:
     result_player_Increase_hp = new_hp_player + hp_player
     
     ai_choice = int(random.randint(1, 2))
-    result_ai_attack = damage_ai - hp_player
+    result_ai_attack = hp_player - damage_ai
     new_hp_ai = int(random.randint(1, 20))
     result_ai_Increase_hp = new_hp_ai + hp_ai
     
@@ -34,6 +34,9 @@ while hp_player or hp_ai <= 0:
     elif ai_choice == 2 :
         print("Your opponent hp has increased to", result_ai_Increase_hp)
         
-            
+    if hp_player or hp_ai <= 0:
+        break
+    else:
+        continue        
     
     
